4bd4e76776c40f84664cef7c419b9d4caa08a0fc
Jessica Lee <wlee.jessica@gmail.com>
2023-11-13 23:43:50 -0800

    Adjusted check_pwd to check for special characters.

--- check_pwd.py
+++ check_pwd.py
@@ -2,9 +2,11 @@ def check_pwd(pwd):
     letters = "abcdefghijklmnopqrstuvwxyz"
     upperLetters = letters.upper()
     numDigits = "0123456789"
+    specialChars = "~`!@#$%^&*()_+-="
     lowercase = False
     uppercase = False
     digits = False
+    special = False
 
     if len(pwd) < 8 or len(pwd) > 20:
         return False
@@ -16,8 +18,10 @@ def check_pwd(pwd):
                 uppercase = True
             if char in numDigits:
                 digits = True
+            if char in specialChars:
+                special = True
         
-        if lowercase == True and uppercase == True and digits == True:
+        if lowercase == True and uppercase == True and digits == True and special == True:
             return True
         else:
             return False
