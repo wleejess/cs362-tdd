36e679ec5f0a0bca58e9ccea540f8b0a24e78f48
Jessica Lee <wlee.jessica@gmail.com>
2023-11-13 23:09:26 -0800

    Fixed check_pwd to check for lower case letters.

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,13 @@
 def check_pwd(pwd):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+
     if len(pwd) < 8 or len(pwd) > 20:
         return False
-    else:
-        return True
+    
+    for char in pwd:
+        if char in letters:
+            return True
+        else:
+            return False
+
+    return True
