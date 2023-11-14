02a20d32d99a75d2a566de5c50680e6bb8c2ccec
Jessica Lee <wlee.jessica@gmail.com>
2023-11-13 23:24:32 -0800

    Adjusted check_pwd to check for lower/upper case letters.

--- check_pwd.py
+++ check_pwd.py
@@ -1,13 +1,25 @@
 def check_pwd(pwd):
     letters = "abcdefghijklmnopqrstuvwxyz"
+    upperLetters = letters.upper()
+    numDigits = "0123456789"
+    lowercase = False
+    uppercase = False
+    digits = False
 
     if len(pwd) < 8 or len(pwd) > 20:
         return False
     
     for char in pwd:
         if char in letters:
-            return True
-        else:
-            return False
+            lowercase = True
+        if char in upperLetters:
+            uppercase = True
+        if char in numDigits:
+            digits = True
+    
+    if lowercase == True and uppercase == True and digits == True:
+        return True
+    else:
+        return False
 
     return True
